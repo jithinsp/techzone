f2d3d2891be30fb85225c365eec8eb3aa1dde7c3
jithinsp <jithinsp001@gmail.com>
2023-07-06 11:32:21 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/techzone/TechzoneApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.techzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/techzone/TechzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.techzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
